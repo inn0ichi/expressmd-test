1622ef5c486df726cb08baad398d0f30a558c070
Marc Angelo Chiapco <marcchiapco34@gmail.com>
2021-12-15 06:06:40 +0800

    registration
    backend complete. need optimization

---
+++ src/pages/App/UserRegistration.js
@@ -0,0 +1,151 @@
+import { Typography, Box, Container, TextField, Button, FormGroup, FormControl, FormHelperText, Avatar, Select, InputLabel, MenuItem } from '@mui/material'
+import firebase from '../../config/firebase';
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
+import './Registration.css';
+
+export default function UserRegistration() {
+    const db = firebase.firestore();
+    const history = useHistory();
+    const [payload, setPayload] = useState({
+        fullname: "",
+        email: "",
+        gender: "",
+        uid: localStorage.getItem("uid"),
+        photoURL: localStorage.getItem("photoURL"),
+        phoneNumber: "",
+        houseNum: "",
+        municipality: "",
+        barangay: "",
+    });
+    const userInput = (prop) => (e) => {
+        setPayload({ ...payload, [prop]: e.target.value });
+    };
+    const completeProfile = (e) => {
+        if (
+            !payload.fullname ||
+            !payload.email ||
+            !payload.gender ||
+            !payload.phoneNumber ||
+            !payload.houseNum ||
+            !payload.municipality ||
+            !payload.barangay
+        ) {
+            alert("Please fill out all of the fields");
+            console.log(payload);
+        } else {
+            db.collection("users")
+                .doc(payload.uid)
+                .set({
+                    fullname: payload.fullname,
+                    email: payload.email,
+                    gender: payload.gender,
+                    uid: payload.uid,
+                    photoURL: payload.photoURL,
+                    phoneNumber: payload.phoneNumber,
+                    location: payload.houseNum + " " + payload.barangay + ", " + payload.municipality,
+                })
+                .then((docRef) => { history.push("/app"); })
+                .catch((error) => {
+                    console.error("Error adding document: ", error);
+                });
+        }
+    };
+    return (
+        <Box>
+            <Container className='registerContainer'>
+                <Typography variant="h4">Complete Your Profile</Typography>
+                <Box className='formContainer'>
+                    <Box className='imgContainer'>
+                        <img className='usrImg' alt='profileImg' src={localStorage.getItem("photoURL")} />
+                    </Box>
+                    <FormGroup>
+                        <FormControl required sx={{ m: 1, minWidth: 120 }}>
+                            <TextField
+                                required
+                                id="filled-required"
+                                label="Fullname"
+                                variant="filled"
+                                onChange={userInput("fullname")}
+                            />
+                        </FormControl>
+                        <FormControl required sx={{ m: 1, minWidth: 120 }}>
+                            <InputLabel >Gender</InputLabel>
+                            <Select
+                                id='gender'
+                                label="Gender *"
+                                value={'Male'}
+                                onChange={userInput("gender")}
+                                value={payload.gender}
+                            >
+                                <MenuItem value={'Male'}>Male</MenuItem>
+                                <MenuItem value={'Female'}>Female</MenuItem>
+                                <MenuItem value={'Others'}>Others/Prefer not to say</MenuItem>
+                            </Select>
+                            <FormHelperText>Required</FormHelperText>
+                        </FormControl>
+                        <FormControl required sx={{ m: 1, minWidth: 120 }}>
+                            <TextField
+                                required
+                                id="filled-required"
+                                label="E-mail"
+                                variant="filled"
+                                onChange={userInput("email")}
+                            />
+                        </FormControl>
+                        <FormControl required sx={{ m: 1, minWidth: 120 }}>
+                            <TextField
+                                required
+                                id="filled-required"
+                                label="Phone Number"
+                                variant="filled"
+                                onChange={userInput("phoneNumber")}
+                                value={payload.phoneNumber}
+                            />
+                        </FormControl>
+                        <FormControl required sx={{ m: 1, minWidth: 120 }}>
+                            <TextField
+                                required
+                                id="filled-required"
+                                label="House # and Street"
+                                variant="filled"
+                                onChange={userInput("houseNum")}
+                                value={payload.houseNum}
+                            />
+                        </FormControl>
+                        <FormControl required sx={{ m: 1, minWidth: 120 }}>
+                            <TextField
+                                required
+                                id="filled-required"
+                                label="Barangay"
+                                variant="filled"
+                                onChange={userInput("barangay")}
+                                value={payload.barangay}
+                            />
+                        </FormControl>
+
+                        <FormControl required sx={{ m: 1, minWidth: 120 }}>
+                            <InputLabel >Municipality</InputLabel>
+                            <Select
+                                id='Municipality'
+                                label="Municipality *"
+                                value={'Bustos'}
+                                onChange={userInput("municipality")}
+                                value={payload.municipality}
+                            >
+                                <MenuItem value={'Bustos'}>Bustos</MenuItem>
+                                <MenuItem value={'Baliuag'}>Baliuag</MenuItem>
+                            </Select>
+                            <FormHelperText>*Required. Bustos and Baliuag only.</FormHelperText>
+                        </FormControl>
+                        <FormControl required sx={{ m: 1, minWidth: 120 }}>
+                            <Button onClick={() => completeProfile()} variant='outlined'>Complete</Button>
+                            <FormHelperText>By clicking complete, you agree to the Privacy Policy.</FormHelperText>
+                        </FormControl>
+
+                    </FormGroup>
+                </Box>
+            </Container>
+        </Box>
+    )
+}
