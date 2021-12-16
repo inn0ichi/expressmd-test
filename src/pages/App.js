a62cf31a422f1493f43d9a177944e51496d235de
Jym-ship-it <fjymcyruz@gmail.com>
2021-12-16 09:08:31 +0800

    home design

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,46 @@
-import { Typography, Box, Container } from '@mui/material'
+import { Typography, Box, Container , Button } from '@mui/material'
 import React from 'react'
 import Nav from '../components/appcomponents/Nav'
+import TopPhoto from "../assets/Drawkit-Vector-Illustration-Medical-01 1.png"
+
+const style = {
+    requestBtn : {
+        
+    },
+
+    textBtn : {
+        display : "flex",
+        flexWrap : "wrap",
+        fontSize : "15px",
+        color : "black",
+        padding : "15px"
+    },
+
+    topPhoto : {
+        height : "200px",
+        width : "125px"
+    },
+
+    topContainer : {
+        display : "flex",
+        justifyContent : "space-between",
+        alignItems : "center"
+
+    }
+}
 
 export default function App() {
     return (
         <Box className='base'>
             <Container>
-                <Typography>Test</Typography>
+                <Box sx = {style.topContainer}>
+                <Button sx = {style.requestBtn} variant = "outlined">
+                    <Typography sx = {style.textBtn}>
+                        "Request a Home visit now"
+                    </Typography>
+                </Button>
+                <Box component = "img" src = {TopPhoto} alt = "" sx = {style.topPhoto}></Box>
+                </Box>
             </Container>
         </Box>
     )
