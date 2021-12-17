067d56233901685f9b060ff2546d98d8b75b3ffa
Marc Angelo Chiapco <marcchiapco34@gmail.com>
2021-12-17 10:13:30 +0800

    update
    home screen added schedule
    added ticker

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,15 @@
-import { Typography, Box, Container, Button , Paper } from '@mui/material'
+import { Typography, Box, Container, Button, Paper } from '@mui/material'
 import Nav from '../components/appcomponents/Nav'
 import TopPhoto from "../assets/Drawkit-Vector-Illustration-Medical-01 1.png"
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { getTheme } from "../redux/actions/uiAction";
+import Ticker from "react-ticker";
+import CampaignIcon from '@mui/icons-material/Campaign';
 
 const style = {
     requestBtn: {
-        borderColor : "white"
+        borderColor: "white"
     },
 
     textBtn: {
@@ -16,28 +18,28 @@ const style = {
         fontSize: "14px",
         color: "white",
         padding: "15px",
-        textAlign : "center"
+        textAlign: "center"
     },
 
     topPhoto: {
         height: "150px",
         width: "110px",
-        marginLeft : "10px"
+        marginLeft: "10px"
     },
 
     topContainer: {
         display: "flex",
-        justifyContent : "center",
+        justifyContent: "center",
         alignItems: "center",
-        marginTop : "20px"
+        marginTop: "20px"
 
     },
-    paperContainer : {
+    paperContainer: {
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
-        padding : "10px",
-        backgroundColor : "#16C2D5"
+        padding: "10px",
+        backgroundColor: "#16C2D5"
     }
 }
 
@@ -50,15 +52,38 @@ export default function App() {
     return (
         <Box className='base'>
             <Container>
-                <Box sx={style.topContainer}>
-                    <Paper sx = {style.paperContainer} elevation={5}>
-                    <Button sx={style.requestBtn} variant="outlined">
-                        <Typography sx={style.textBtn}>
-                            "Request a Home visit now"
-                        </Typography>
-                    </Button>
-                    <Box component="img" src={TopPhoto} alt="" sx={style.topPhoto}></Box>
+                <Box >
+                    <Paper >
+                        <Ticker mode="await" speed='4' offset='25'>
+                            {({ index }) => (
+                                <>
+                                    <Box className='ticker'>
+                                        <CampaignIcon />
+                                        <Typography variant="subtitle2">Sample Announcement! This is a test! </Typography>
+                                    </Box>
+
+                                </>
+                            )}
+                        </Ticker>
                     </Paper>
+                </Box>
+                <Box sx={style.topContainer}>
+                    <Paper sx={style.paperContainer} elevation={5}>
+                        <Button disabled sx={style.requestBtn} variant="outlined">
+                            <Typography sx={style.textBtn}>
+                                Welcome to ExpressMD
+                            </Typography>
+                        </Button>
+                        <Box component="img" src={TopPhoto} alt="" sx={style.topPhoto}></Box>
+                    </Paper>
+                </Box>
+                <Box>
+                    <Box>
+                        <Typography variant='h6'>Scheduled Appointment</Typography>
+                        <Typography variant="subtitle2">There is no scheduled appointment.</Typography>
+                        <Button variant='outlined'>Set an appointment now</Button>
+                    </Box>
+
                 </Box>
             </Container>
         </Box>
