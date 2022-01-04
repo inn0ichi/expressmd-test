d00e33558f34bbdf880aab860b604c36fccc011e
jeron-beep <78426597+jeron-beep@users.noreply.github.com>
2022-01-05 03:10:26 +0800

    Update Request.js

--- src/pages/App/Request.js
+++ src/pages/App/Request.js
@@ -1,12 +1,15 @@
-import React from "react";
-import { Typography, Box, TextField } from "@mui/material";
+import React,{ useState, useEffect } from "react";
+import { Typography, Box, TextField, Button } from "@mui/material";
 import FormLabel from "@mui/material/FormLabel";
 import FormControl from "@mui/material/FormControl";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormHelperText from "@mui/material/FormHelperText";
 import Checkbox from "@mui/material/Checkbox";
-
+import firebase from '../../config/firebase';
+import { useDispatch } from 'react-redux';
+import { getTheme } from "../../redux/actions/uiAction"
+import { useHistory } from "react-router-dom";
 const style = {
   parentCon : {
     display : "flex",
@@ -44,15 +47,83 @@ const style = {
   }
 }
 
+
 export default function Request() {
+  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+      dispatch(getTheme());
+  }, [dispatch]);
+
+  const db = firebase.firestore();
+  const history = useHistory();
+  const [payload, setPayload] = useState({
+      Feel: "",
+      Anysymptoms : "",
+      others: "",
+  });
+  const userInput = (prop) => (e) => {
+    setPayload({ ...payload, [prop]: e.target.value });
+};
+
+const submitForm = (e) => {
+  getValue();
+  if (
+      !payload.Feel
+     
+  ) {
+      alert("Please enter the required fields!");
+      console.log(payload);
+  } else {
+
+      db.collection("userSymptoms")
+          .doc(payload.uid)
+          .set({
+            
+              Feel: payload.Feel,
+              Anysymptoms: payload.Anysymptoms,
+              others: payload.others,
+          })
+          .then((docRef) => { history.push("/"); })
+          .catch((error) => {
+              console.error("Error adding document: ", error);
+          });
+  }
+};
+
+function getValue() {
+
+  var checks = document.getElementsByName('checks')
+  var str='';
+
+  for (var i = 0; i < 8; i++){
+
+  if (checks[i].checked === true){
+    str += checks[i].value + ", ";
+  }
+
+}
+payload.Anysymptoms=str;
+}
+
+ 
+  
+
+
+
+
+
+
   return (
     <Box className="base">
-
       <Box sx = {style.parentCon}>
         <Typography sx = {style.label}>What do you feel ? </Typography>
         <Typography sx = {style.subLabel}>*Required</Typography>
       </Box>
       <Box sx = {style.inputField}>
+   
+                  
         <TextField
           id="outlined-basic"
           variant="outlined"
@@ -60,7 +131,9 @@ export default function Request() {
           maxRows={10}
           minRows={6}
           sx = {style.textField}
+          onChange={userInput("Feel")}
         />
+      
       </Box>
       <Box>
         <Box
@@ -72,42 +145,53 @@ export default function Request() {
         >
           <FormControl sx={{ m: 1 }} component="fieldset" variant="standard">
             <Typography>Any Symptoms ?</Typography>
+      
             <FormGroup sx = {style.checkCon}>
               <Box sx = {style.checkBox}>
               <FormControlLabel
-                control={<Checkbox name="Cough" />}
+                control={<Checkbox name="checks" 
+                onChange={userInput("Anysymptoms")}/>}
                 label="Cough"
+                value="Cough"
               />
               <FormControlLabel
-                control={<Checkbox name="Fever" />}
+                control={<Checkbox name="checks"  onChange={userInput("Anysymptoms")}/>}
                 label="Fever"
+               value="Fever"
               />
               <FormControlLabel
-                control={<Checkbox name="Fatigue" />}
+                control={<Checkbox name="checks" onChange={userInput("Anysymptoms")}/>}
                 label="Fatigue"
+                value="Fatigue"
               />
               <FormControlLabel
-                control={<Checkbox name="Muscle or Body Aches" />}
+                control={<Checkbox name="checks" onChange={userInput("Anysymptoms")}/>}
+               value="Muscle or Body Aches"
                 label="Muscle or Body Aches"
               />
               </Box>
               <Box sx = {style.checkBox}>
               <FormControlLabel
-                control={<Checkbox name="Shortness of Breath" />}
+                control={<Checkbox name="checks" onChange={userInput("Anysymptoms")}/>}
+               value="Shortness of Breath"
                 label="Shortness of Breath"
               />
               <FormControlLabel
-                control={<Checkbox name="Headache" />}
+                control={<Checkbox name="checks" onChange={userInput("Anysymptoms")}/>}
                 label="Headache"
+              value="Headache"
               />
               <FormControlLabel
-                control={<Checkbox name="Sore Throat" />}
+                control={<Checkbox name="checks" onChange={userInput("Anysymptoms")}/>}
+              value="Sore Throat"
                 label="Sore Throat"
               />
               <FormControlLabel
-                control={<Checkbox name="Diarrhea" />}
+                control={<Checkbox name="checks" onChange={userInput("Anysymptoms")}/>}
+                value="Diarrhea"
                 label="Diarrhea"
               />
+             
               </Box>
             </FormGroup>
           </FormControl>
@@ -121,8 +205,13 @@ export default function Request() {
           required
           id="standard-required"
           defaultValue=""
-          variant="standard"/>
+          variant="standard"
+          onChange={userInput("others")}
+         />
       </Box>
+      
+     
+      <Button onClick={() => submitForm()} variant='outlined'>Submit</Button>
     </Box>
   );
 }
