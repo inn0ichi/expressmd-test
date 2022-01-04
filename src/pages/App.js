da7b3327b638be45cd0abf798e8eb5135b462050
Jym-ship-it <fjymcyruz@gmail.com>
2022-01-04 18:45:58 +0800

    update

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,166 +1,187 @@
-import { Typography, Box, Container, Button, Paper } from '@mui/material'
-import Nav from '../components/appcomponents/Nav'
-import TopPhoto from "../assets/Drawkit-Vector-Illustration-Medical-01 1.png"
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux';
+import { Typography, Box, Container, Button, Paper } from "@mui/material";
+import Nav from "../components/appcomponents/Nav";
+import TopPhoto from "../assets/Drawkit-Vector-Illustration-Medical-01 1.png";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { getTheme } from "../redux/actions/uiAction";
 import Ticker from "react-ticker";
-import CampaignIcon from '@mui/icons-material/Campaign';
-import category from "../assets/child 1.png"
-import doctorPhoto from "../assets/doctor 1.png"
-import { borderRadius, fontWeight } from '@mui/system';
-import { NavLink } from 'react-router-dom';
+import CampaignIcon from "@mui/icons-material/Campaign";
+import category from "../assets/child 1.png";
+import doctorPhoto from "../assets/doctor 1.png";
+import { borderRadius, fontWeight } from "@mui/system";
+import { NavLink } from "react-router-dom";
 
 const style = {
-    requestBtn: {
-        borderColor: "white"
+  requestBtn: {
+    borderColor: "white",
+  },
+
+  textBtn: {
+    display: "flex",
+    flexWrap: "wrap",
+    fontSize: "14px",
+    color: "white",
+    padding: "15px",
+    textAlign: "center",
+  },
+
+  topPhoto: {
+    height: "150px",
+    width: "110px",
+    marginLeft: "10px",
+  },
+
+  topContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "20px",
+  },
+  paperContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "10px",
+    backgroundColor: "#16C2D5",
+  },
+
+  wrapper: {
+    marginTop: "30px",
+    maxHeight: "80px",
+    display: "flex",
+    overflowX: "auto",
+    "-webkit-scrollbar": {
+      display: "none",
     },
+    "-ms-overflow-style": "none",
+  },
 
-    textBtn: {
-        display: "flex",
-        flexWrap: "wrap",
-        fontSize: "14px",
-        color: "white",
-        padding: "15px",
-        textAlign: "center"
-    },
+  categoryPaper: {
+    minWidth: "200px",
+    height: "80px",
+    borderColor: "#7EB6BC",
+    borderWidth: "2px",
+    marginRight: "20px",
+    borderRadius: "8px",
+    display: "flex",
+    justifyContent: "center",
+  },
 
-    topPhoto: {
-        height: "150px",
-        width: "110px",
-        marginLeft: "10px"
-    },
+  item: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
 
-    topContainer: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: "20px"
+  category: {
+    height: "50px",
+    width: "50px",
+    alignItems: "center",
+    padding: "5px",
+    borderRadius: "3px",
+  },
+  categoryText: {
+    fontSize: "14px",
+    marginLeft: "8px",
+  },
 
-    },
-    paperContainer: {
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: "10px",
-        backgroundColor: "#16C2D5"
-    },
-
-    wrapper: {
-        marginTop: "30px",
-        maxHeight: "80px",
-        display: "flex",
-        overflowX: "auto",
-        "-webkit-scrollbar": {
-            display: "none"
-        },
-        "-ms-overflow-style": "none",
-
-    },
-
-    categoryPaper: {
-        minWidth: "200px",
-        height: "80px",
-        borderColor: "#7EB6BC",
-        borderWidth: "2px",
-        marginRight: "20px",
-        borderRadius: "8px",
-        display: "flex",
-        justifyContent: "center",
-
-    },
-
-    item: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "row"
-
-    },
-
-    category: {
-        height: "50px",
-        width: "50px",
-        alignItems: "center",
-        padding: "5px",
-        borderRadius: "3px"
-    },
-    categoryText: {
-        fontSize: "14px",
-        marginLeft: "8px"
-    },
-
-    label: {
-        marginTop: "20px"
-    },
-
-}
+  label: {
+    marginTop: "20px",
+  },
+};
 
 export default function App() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getTheme());
-    }, [dispatch]);
-    return (
-        <Box className='base'>
-            <Container>
-                <Box className='tickerBox'>
-                    <Paper>
-                        <Ticker mode="await" speed='4' offset='25'>
-                            {({ index }) => (
-                                <>
-                                    <Box className='ticker'>
-                                        <CampaignIcon />
-                                        <Typography variant="subtitle2">Sample Announcement! This is a test! </Typography>
-                                    </Box>
+  useEffect(() => {
+    dispatch(getTheme());
+  }, [dispatch]);
+  return (
+    <Box className="base">
+      <Container>
+        <Box className="tickerBox">
+          <Paper>
+            <Ticker mode="await" speed="4" offset="25">
+              {({ index }) => (
+                <>
+                  <Box className="ticker">
+                    <CampaignIcon />
+                    <Typography variant="subtitle2">
+                      Sample Announcement! This is a test!{" "}
+                    </Typography>
+                  </Box>
+                </>
+              )}
+            </Ticker>
+          </Paper>
+        </Box>
+        <Box sx={style.topContainer}>
+          <Paper sx={style.paperContainer} elevation={5}>
+            <Button disabled sx={style.requestBtn} variant="outlined">
+              <Typography sx={style.textBtn}>Welcome to ExpressMD</Typography>
+            </Button>
+            <Box
+              component="img"
+              src={TopPhoto}
+              alt=""
+              sx={style.topPhoto}
+            ></Box>
+          </Paper>
+        </Box>
+        <Box className="schedBox">
+          <Container>
+            <Paper elevation={3} className="schedPaper">
+              <Typography className="schedHeader" variant="h6">
+                Scheduled Appointment
+              </Typography>
+              <Box className="schedDetails">
+                <Typography className="schedText" variant="subtitle2">
+                  There is no scheduled appointment.
+                </Typography>
+                <NavLink to="/search">
+                  <Button className="schedButton" variant="contained">
+                    Set an appointment now
+                  </Button>
+                </NavLink>
+              </Box>
+            </Paper>
+          </Container>
+        </Box>
 
-                                </>
-                            )}
-                        </Ticker>
-                    </Paper>
-                </Box>
-                <Box sx={style.topContainer}>
-                    <Paper sx={style.paperContainer} elevation={5}>
-                        <Button disabled sx={style.requestBtn} variant="outlined">
-                            <Typography sx={style.textBtn}>
-                                Welcome to ExpressMD
-                            </Typography>
-                        </Button>
-                        <Box component="img" src={TopPhoto} alt="" sx={style.topPhoto}></Box>
-                    </Paper>
-                </Box>
-                <Box className='schedBox'>
-                    <Container>
-                        <Paper elevation={3} className='schedPaper'>
-                            <Typography className='schedHeader' variant='h6'>Scheduled Appointment</Typography>
-                            <Box className='schedDetails'>
-                                <Typography className='schedText' variant="subtitle2">There is no scheduled appointment.</Typography>
-                                <NavLink to='/search'>
-                                    <Button className='schedButton' variant='contained'>Set an appointment now</Button>
-                                </NavLink>
-                            </Box>
-                        </Paper>
-                    </Container>
-                </Box>
-
-                <Box sx={style.label}>
-                    <Typography variant='h6'>Doctor Category</Typography>
-                </Box>
-                <Box sx={style.wrapper}>
-                    <Paper sx={style.categoryPaper} variant="outlined">
-                        <Box sx={style.item}>
-                            <Box component="img" src={doctorPhoto} alt="" sx={style.category} style={{ backgroundColor: "#FFC107" }}></Box>
-                            <Typography variant="subtitle2" sx={style.categoryText}>General Doctor</Typography>
-                        </Box>
-                    </Paper>
-                    <Paper sx={style.categoryPaper} variant="outlined">
-                        <Box sx={style.item}>
-                            <Box component="img" src={category} alt="" sx={style.category} style={{ backgroundColor: "#65A4DA" }}></Box>
-                            <Typography variant="subtitle2" sx={style.categoryText} >Pediatrics</Typography>
-                        </Box>
-                    </Paper>
-                    {/* <Paper sx = {style.categoryPaper} variant = "outlined">
+        <Box sx={style.label}>
+          <Typography variant="h6">Doctor Category</Typography>
+        </Box>
+        <Box sx={style.wrapper}>
+          <Paper sx={style.categoryPaper} variant="outlined">
+            <Box sx={style.item}>
+              <Box
+                component="img"
+                src={doctorPhoto}
+                alt=""
+                sx={style.category}
+                style={{ backgroundColor: "#FFC107" }}
+              ></Box>
+              <Typography variant="subtitle2" sx={style.categoryText}>
+                General Doctor
+              </Typography>
+            </Box>
+          </Paper>
+          <Paper sx={style.categoryPaper} variant="outlined">
+            <Box sx={style.item}>
+              <Box
+                component="img"
+                src={category}
+                alt=""
+                sx={style.category}
+                style={{ backgroundColor: "#65A4DA" }}
+              ></Box>
+              <Typography variant="subtitle2" sx={style.categoryText}>
+                Pediatrics
+              </Typography>
+            </Box>
+          </Paper>
+          {/* <Paper sx = {style.categoryPaper} variant = "outlined">
                     <Box sx = {style.item}>
                         <Typography variant="subtitle2" sx = {style.categoryText}>Pediatrics</Typography>
                     </Box>
@@ -170,8 +191,8 @@ export default function App() {
                     category 4
                     </Box>
                     </Paper> */}
-                </Box>
-            </Container>
         </Box>
-    )
+      </Container>
+    </Box>
+  );
 }
--- src/pages/App/Request.js
+++ src/pages/App/Request.js
@@ -1,86 +1,78 @@
-import React from 'react';
-import { Typography, Box , TextField} from '@mui/material';
-import FormLabel from '@mui/material/FormLabel';
-import FormControl from '@mui/material/FormControl';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormHelperText from '@mui/material/FormHelperText';
-import Checkbox from '@mui/material/Checkbox';
+import React from "react";
+import { Typography, Box, TextField } from "@mui/material";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormHelperText from "@mui/material/FormHelperText";
+import Checkbox from "@mui/material/Checkbox";
 export default function Request() {
-
-    return (
-        <Box className='base'>
-            <Box>
-                <Typography variant="label">Request Appointment</Typography>
-            </Box>
-            <Box>
-                <Typography>What do you feel ? <Typography>*required</Typography></Typography>
-            </Box>
-            <Box>
-                <TextField id="outlined-basic" variant="outlined"  multiline maxRows={4} minRows={6} />
-            </Box>
-            <Box>
-                <Box sx={{ display: 'flex' , flexDirection : "column" , justifyContent : "space-between" }}>
-                    <FormControl sx={{ m: 1 }} component="fieldset" variant="standard">
-                    <Typography>Any Symptoms ?</Typography>
-                    <FormGroup>
-                    <FormControlLabel
-                        control={
-                        <Checkbox  name="Cough" />
-                        }
-                    label="Cough"
-                    />
-                    <FormControlLabel
-                        control={
-                        <Checkbox  name="Fever" />
-                        }
-                    label="Fever"
-                    />
-                    <FormControlLabel
-                        control={
-                        <Checkbox  name="Fatigue" />
-                        }
-                    label="Fatigue"
-                    />
-                    <FormControlLabel
-                        control={
-                        <Checkbox  name="Muscle or Body Aches" />
-                    }
-                    label="Muscle or Body Aches"
-                    />
-                    <FormControlLabel
-                        control={
-                        <Checkbox  name="Shortness of Breath" />
-                    }
-                    label="Shortness of Breath"
-                    />
-                    <FormControlLabel
-                        control={
-                        <Checkbox  name="Headache" />
-                    }
-                    label="Headache"
-                    />
-                    <FormControlLabel
-                        control={
-                        <Checkbox  name="Sore Throat" />
-                    }
-                    label="Sore Throat"
-                    />
-                    <FormControlLabel
-                        control={
-                        <Checkbox  name="Diarrhea" />
-                    }
-                    label="Diarrhea"
-                    />
-                    </FormGroup>
-                    </FormControl>
-                    </Box>
-                    
-
-                </Box>
-
-
-            
-        </Box >
-    )
+  return (
+    <Box className="base">
+      <Box>
+        <Typography variant="label">Request Appointment</Typography>
+      </Box>
+      <Box>
+        <Typography>
+          What do you feel ? <Typography>*required</Typography>
+        </Typography>
+      </Box>
+      <Box>
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          multiline
+          maxRows={4}
+          minRows={6}
+        />
+      </Box>
+      <Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <FormControl sx={{ m: 1 }} component="fieldset" variant="standard">
+            <Typography>Any Symptoms ?</Typography>
+            <FormGroup>
+              <FormControlLabel
+                control={<Checkbox name="Cough" />}
+                label="Cough"
+              />
+              <FormControlLabel
+                control={<Checkbox name="Fever" />}
+                label="Fever"
+              />
+              <FormControlLabel
+                control={<Checkbox name="Fatigue" />}
+                label="Fatigue"
+              />
+              <FormControlLabel
+                control={<Checkbox name="Muscle or Body Aches" />}
+                label="Muscle or Body Aches"
+              />
+              <FormControlLabel
+                control={<Checkbox name="Shortness of Breath" />}
+                label="Shortness of Breath"
+              />
+              <FormControlLabel
+                control={<Checkbox name="Headache" />}
+                label="Headache"
+              />
+              <FormControlLabel
+                control={<Checkbox name="Sore Throat" />}
+                label="Sore Throat"
+              />
+              <FormControlLabel
+                control={<Checkbox name="Diarrhea" />}
+                label="Diarrhea"
+              />
+            </FormGroup>
+          </FormControl>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
