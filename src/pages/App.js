fcc21c8c691883f651abf7f6a19231bb55fc992a
Marc Angelo Chiapco <marcchiapco34@gmail.com>
2022-01-31 23:13:26 +0800

    Update App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -180,6 +180,20 @@ export default function App() {
     topdoc: [],
   });
 
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      var uid = user.uid;
+      localStorage.setItem("uid", uid);
+      // ...
+    } else {
+      // User is signed out
+      // ...
+      setisLoggedOut(true);
+    }
+  });
+
   useEffect(() => {
     let isSubscribed = true;
     getAuth().onAuthStateChanged(function (user) {
