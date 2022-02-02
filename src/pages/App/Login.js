bdc3ae9ae31d52d775377b4736341ab15972e50d
Marc Angelo Chiapco <marcchiapco34@gmail.com>
2022-02-02 09:31:52 +0800

    Update Login.js

--- src/pages/App/Login.js
+++ src/pages/App/Login.js
@@ -43,6 +43,16 @@ function Login() {
             }
         })
     }); */
+
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/firebase.User
+            var uid = user.uid;
+            localStorage.setItem("uid", uid);
+            history.push("/");
+        }
+    });
     const [emailError, setEmailError] = useState("")
     const userInputEmail = (prop) => (e) => {
         setPayload({ ...payload, [prop]: e.target.value });
